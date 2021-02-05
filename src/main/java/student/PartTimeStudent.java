83e842580df56e365b508cea776eb5e52155a77b
ryantex <ryan.m.tex@gmail.com>
2021-02-05 10:32:04 -0500

    added part-time student

---
+++ src/main/java/student/PartTimeStudent.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author ryanteixeira
+ */
+public class PartTimeStudent extends Student {
+    
+    private int numCourses; 
+    
+    public PartTimeStudent(int numofC){
+        numCourses=numofC;
+        
+    }
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+    
+}
